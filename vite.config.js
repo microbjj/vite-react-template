7b4e18b6285c9020d60f72ce43fa9f7714e7ea56
microbjj <1705888@mail.ru>
2025-05-07 13:38:25 +0700

    add Tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import eslint from "vite-plugin-eslint"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
     plugins: [
@@ -10,5 +11,6 @@ export default defineConfig({
             exclude: ["node_modules", "dist"],
             cache: true,
         }),
+        tailwindcss(),
     ],
 })
